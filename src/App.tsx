9627ee650658480e373b2439a676e765d37052d0
Faber-Herrera <faberherrera.ia@gmail.com>
2024-10-25 18:40:59 -0500

    add post

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import useHttpData from './hooks/useFetchData';
+import useHttpData from './hooks/useHttpData';
 
 type User = {
-  id: string;
+  id?: string;
   name: string;
 };
 
 function App() {
   const url = 'https://jsonplaceholder.typicode.com/users';
-  const { data: users, loader, error } = useHttpData<User>(url);
+  const { data: users, loader, error, addData: addUser } = useHttpData<User>(url);
 
   return (
     <>
       {loader === 'pending' ? <span>Cargando...</span> : null}
       {loader === 'success' && users.length > 0 ? (
         <ul>
+          <button onClick={() => addUser({ name: 'Canchito feliz' })}>Enviar</button>
           {users.map((u: User) => (
             <li key={u.id}>{u.name}</li>
           ))}
--- src/hooks/useHttpData.tsx
+++ src/hooks/useHttpData.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 type Loader = 'pending' | 'success' | 'error';
 
-export default function useFetchData<T>(url: string) {
+export default function useHttpData<T>(url: string) {
   const [data, setData] = useState<T[]>([]);
   const [loader, setLoader] = useState<Loader>('pending');
   const [error, setError] = useState<string>();
@@ -29,5 +29,29 @@ export default function useFetchData<T>(url: string) {
     return () => controller.abort();
   }, []);
 
-  return { data, loader, error };
+  const addData = async (element: T) => {
+    const initialData = [...data];
+    setData([{ id: 0, ...element }, ...data]);
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(element),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        setData(initialData);
+        throw new Error(`${response.status}`);
+      }
+
+      const savedData = await response.json();
+      setData([savedData, ...initialData]);
+    } catch (error) {
+      setError((error as Error).message);
+    }
+  };
+
+  return { data, loader, error, addData };
 }
