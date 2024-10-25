ef50b0b6349843397f5fccb9ecb1a903e370066c
Faber-Herrera <faberherrera.ia@gmail.com>
2024-10-25 17:35:59 -0500

    antes de pasar a async await

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import useUsers from './hooks/useUsers';
+
 function App() {
-  return <div>Hola papu</div>;
+  const { users, loader, error } = useUsers();
+  return (
+    <>
+      {loader === 'pending' ? <span>Cargando...</span> : null}
+      {loader === 'success' && users.length > 0 ? (
+        <ul>
+          {users.map((u) => (
+            <li key={u.id}>{u.name}</li>
+          ))}
+        </ul>
+      ) : null}
+      {loader === 'error' ? <span>Ocurrió un error: {error}</span> : null}
+    </>
+  );
 }
 
 export default App;
